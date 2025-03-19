9187e9752b8113811911d735ef854d98c6d93682
Golddirio <e1156147@u.nus.edu>
2025-03-19 11:25:48 +0800

    Fix checkstyle errors again

--- src/test/java/seedu/address/logic/parser/FilterNoteCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/FilterNoteCommandParserTest.java
@@ -1,19 +1,20 @@
 package seedu.address.logic.parser;
 
-import org.junit.jupiter.api.Test;
-import seedu.address.logic.commands.FilterNoteCommand;
-
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_NOTE_TITLE;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PATIENT;
 
+import org.junit.jupiter.api.Test;
+
+import seedu.address.logic.commands.FilterNoteCommand;
 public class FilterNoteCommandParserTest {
-    private FilterNoteCommandParser testParser= new FilterNoteCommandParser();
+    private FilterNoteCommandParser testParser = new FilterNoteCommandParser();
     @Test
     public void parse_emptyArg_throwsParseException() {
-        assertParseFailure(testParser, "     ", String.format(MESSAGE_INVALID_COMMAND_FORMAT, FilterNoteCommand.MESSAGE_USAGE));
+        assertParseFailure(testParser, "     ",
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, FilterNoteCommand.MESSAGE_USAGE));
     }
 
     @Test

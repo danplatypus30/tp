1ed42bc5f2f7ac6c25063e5e0bacd5d06a8f6e00
danplatypus30 <daniel.gohch@hotmail.com>
2025-03-20 21:32:37 +0800

    Fix checkstyle errors

--- src/main/java/seedu/address/logic/commands/EditNoteCommand.java
+++ src/main/java/seedu/address/logic/commands/EditNoteCommand.java
@@ -13,7 +13,6 @@ import java.util.TreeSet;
 
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.Messages;
-import seedu.address.logic.commands.EditCommand.EditPatientDescriptor;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.note.Note;

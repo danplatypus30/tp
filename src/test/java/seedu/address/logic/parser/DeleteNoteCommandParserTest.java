c54c0700f1ab6081f52b02754bc48c2078b62250
fieash <wongyuzhang45@gmail.com>
2025-03-20 22:00:26 +0800

    Remove unused imports

--- src/test/java/seedu/address/logic/parser/DeleteNoteCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/DeleteNoteCommandParserTest.java
@@ -1,7 +1,6 @@
 package seedu.address.logic.parser;
 
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
-import static seedu.address.logic.Messages.MESSAGE_INVALID_NOTE_DELETE_TITLE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_NOTE_TITLE;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
@@ -11,7 +10,6 @@ import org.junit.jupiter.api.Test;
 
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.DeleteNoteCommand;
-import seedu.address.model.note.Note;
 
 public class DeleteNoteCommandParserTest {
 

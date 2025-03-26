744bbbda64f2b43142d00f9d6caabd0c8c3911c0
fieash <wongyuzhang45@gmail.com>
2025-03-27 00:30:26 +0800

    Remove unused import

--- src/main/java/seedu/address/logic/commands/DeleteNoteCommand.java
+++ src/main/java/seedu/address/logic/commands/DeleteNoteCommand.java
@@ -1,7 +1,6 @@
 package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
-import static seedu.address.logic.commands.EditCommand.MESSAGE_DUPLICATE_PATIENT;
 import static seedu.address.logic.commands.EditCommand.createEditedPatient;
 import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PATIENTS;
 

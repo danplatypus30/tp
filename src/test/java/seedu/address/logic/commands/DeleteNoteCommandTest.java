04e1eecabae427e25127a0bcde567e4709993ec1
fieash <wongyuzhang45@gmail.com>
2025-03-27 01:24:58 +0800

    Remove unused imports

--- src/test/java/seedu/address/logic/commands/DeleteNoteCommandTest.java
+++ src/test/java/seedu/address/logic/commands/DeleteNoteCommandTest.java
@@ -1,30 +1,21 @@
 package seedu.address.logic.commands;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.logic.commands.CommandTestUtil.NON_EXISTENT_NOTE_TITLE;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
-import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PATIENT;
 import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_PATIENT;
 import static seedu.address.testutil.TypicalPatients.getTypicalAddressBook;
 
-import java.util.TreeSet;
-
 import org.junit.jupiter.api.Test;
 
 import seedu.address.commons.core.index.Index;
-import seedu.address.logic.Messages;
 import seedu.address.logic.commands.exceptions.CommandException;
-import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
-import seedu.address.model.note.Note;
-import seedu.address.model.patient.Patient;
-import seedu.address.testutil.PatientBuilder;
 
 public class DeleteNoteCommandTest {
     private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
